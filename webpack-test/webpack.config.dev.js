08018d31f577c7943bd1b44b32f58380a66b58e6
maxim_pavlov <maxim_pavlov@epam.com>
2021-09-22 10:49:05 +0300

    added cross-env and webpack-merge

--- webpack-test/webpack.config.dev.js
+++ webpack-test/webpack.config.dev.js
@@ -1,38 +1,20 @@
-const path = require("path");
-const HtmlWebpackPlagin = require("html-webpack-plugin");
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-    ],
-  },
-  resolve: {
-    modules: [path.resolve(__dirname), "node_modules"],
-    extensions: ["*", ".js", ".jsx"],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname),
+module.exports = merge(common, {
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js',
     },
-    compress: true,
-    port: 3000,
-  },
-  watch: true,
-  devtool: "source-map",
-  plugins: [
-    new HtmlWebpackPlagin({
-      template: "./src/index.html",
-    }),
-  ],
-};
+    devtool: 'source-map',
+    watch: true,
+    devServer: {
+        static: {
+            directory: path.join(__dirname),
+        },
+        compress: true,
+        port: 3000,
+    },
+});
--- webpack-test/webpack.config.prod.js
+++ webpack-test/webpack.config.prod.js
@@ -1,36 +1,18 @@
-const path = require("path");
-const HtmlWebpackPlagin = require("html-webpack-plugin");
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "/public"),
-    filename: "index_bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-    ],
-  },
-  resolve: {
-    modules: [path.resolve(__dirname), "node_modules"],
-    extensions: ["*", ".js", ".jsx"],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname),
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        path: path.join(__dirname, '/public'),
+        filename: 'index_bundle.js',
     },
-    compress: true,
-    port: 9000,
-  },
-  plugins: [
-    new HtmlWebpackPlagin({
-      template: "./src/index.html",
-    }),
-  ],
-};
+    devServer: {
+        static: {
+            directory: path.join(__dirname),
+        },
+        compress: true,
+        port: 9000,
+    },
+});
